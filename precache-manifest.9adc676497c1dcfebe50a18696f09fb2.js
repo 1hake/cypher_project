6e36b3fde1a957ff7d8b4bf67d6e9bba4e46c255
Colin Champdavoine <colinchampdavoine@Colins-MacBook-Pro.local>
2020-01-06 16:17:28 +0100

    Updates

---
+++ precache-manifest.9adc676497c1dcfebe50a18696f09fb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513ce16808e985e53084e7d1177909b7",
+    "url": "/cypher_project/index.html"
+  },
+  {
+    "revision": "82dfd635add57e0456ad",
+    "url": "/cypher_project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b483b26b18787d1f7a25",
+    "url": "/cypher_project/static/js/2.5d00b276.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cypher_project/static/js/2.5d00b276.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82dfd635add57e0456ad",
+    "url": "/cypher_project/static/js/main.6ae49051.chunk.js"
+  },
+  {
+    "revision": "3ed33e2d2263414bf4c5",
+    "url": "/cypher_project/static/js/runtime-main.f1d11d32.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cypher-project/precache-manifest.22859eb7166e9f77ee538036ffa49dbd.js"
+  "/cypher_project/precache-manifest.9adc676497c1dcfebe50a18696f09fb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cypher-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cypher_project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
